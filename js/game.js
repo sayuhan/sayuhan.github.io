c9eca7515bc2b2f035e3094bb31d8a1c39b89c12
sayuhan <68998354+sayuhan@users.noreply.github.com>
2020-07-30 21:27:43 +0900

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,30 @@
+
+function guu{
+    var rand =Math.floor(Math.random()*Math.floor(2));
+    if (rand == 0){
+        alert("相手はチョキを出しました。あなたの勝ちです");
+    }if (rand == 1){
+        alert("相手はパーを出しました。あなたの負けです");
+    }if (rand == 2){
+        alert("相手はグーを出しました。あいこです");
+    }
+}
+function tyokki{
+    var rand =Math.floor(Math.random()*Math.floor(2));
+    if (rand == 0){
+        alert("相手はパーを出しました。あなたの勝ちです");
+    }if (rand == 1){
+        alert("相手はグーを出しました。あなたの負けです");
+    }if (rand == 2){
+        alert("相手はチョキを出しました。あいこです");
+    }
+}function paa{
+    var rand = Math.floor(Math.random()*Math.floor(2));
+    if (rand == 0){
+        alert("相手はチョキを出しました。あなたの勝ちです");
+    }if (rand == 1){
+        alert("相手はパーを出しました。あなたの負けです");
+    }if (rand == 2){
+        alert("相手はグーを出しました。あいこです");
+    }
+}
